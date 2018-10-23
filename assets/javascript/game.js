b810a80802d7c8d6a602f4b17393ae6bf61f5034
unknown <brant_keener@yahoo.com>
2018-10-22 21:27:08 -0600

    build initial html, and css reset.

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+
+let character = {
+    picture : [insertLinksHere],
+    name : ["Luke Skywalker", "Rey", "Obi-Wan Kenobi", "Chewbacca", "Darth Vader", "Darth Sidious", "Captain Phasma", "General Grievous"],
+    attackPower : [],
+    counterAttack : [],
+}
+
