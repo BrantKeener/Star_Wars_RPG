c07026f3cb771c3dada555c1441b8c04dfad0342
unknown <brant_keener@yahoo.com>
2018-10-23 19:48:07 -0600

    Add images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,52 @@
 
-let character = {
-    picture : [insertLinksHere],
-    name : ["Luke Skywalker", "Rey", "Obi-Wan Kenobi", "Chewbacca", "Darth Vader", "Darth Sidious", "Captain Phasma", "General Grievous"],
-    attackPower : [],
-    counterAttack : [],
-}
+let character = [
+    {
+    picture : insertLinksHere,
+    name : "Luke Skywalker",
+    attackPower : 100,
+    counterAttack : 100,
+    },
+    {
+    picture : insertLinksHere,
+    name : "Rey",
+    attackPower : 100,
+    counterAttack : 100,
+    },
+    {
+    picture : insertLinksHere,
+    name : "Obi-Wan Kenobi",
+    attackPower : 100,
+    counterAttack : 100,
+    },
+    {
+    picture : insertLinksHere,
+    name : "Chewbacca",
+    attackPower : 100,
+    counterAttack : 100,
+    },
+    {
+    picture : insertLinksHere,
+    name : "Darth Vader",
+    attackPower : 100,
+    counterAttack : 100,
+    },
+    {
+    picture : insertLinksHere,
+    name : "Darth Sidious",
+    attackPower : 100,
+    counterAttack : 100,
+    },
+    {
+    picture : insertLinksHere,
+    name : "Captain Phasma",
+    attackPower : 100,
+    counterAttack : 100,
+    },
+    {
+    picture : insertLinksHere,
+    name : "General Grievous",
+    attackPower : 100,
+    counterAttack : 100,
+    }
+]
 
